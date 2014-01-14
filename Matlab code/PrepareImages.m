4041dbb0b356ccac1d6105e4b448290de282117e
Rowan Doble <rowandoble@gmail.com>
2014-01-14 12:23:10 +0000

    Simplifed PrepareImages as can now fit all images in memory so don't need to do in batches.
    Also removed downsizing from WarpImages as not necessary - if want to downsize at some point then can do in a separate function

--- Matlab code/PrepareImages.m
+++ Matlab code/PrepareImages.m
@@ -1,19 +1,8 @@
-function [ allWarped, mask ] = PrepareImages( startIndex, endIndex, landmarkLocations, M, T )
+function [ textures, mask ] = PrepareImages( images, landmarkLocations, M, T )
     m2d = MtoMean2D(M);
 
     [ alphas, betas, triX, mask, xmin, ymin, npix ] = InitialisePieceWiseAffine(T, m2d);
-
-    % Have to use matfile as can't fit all images in at once, so do it in 2
-    % batches
-    mpie = matfile('C:\dataset\mpie-frontal.mat');
-    % An array of vectorised, warped images that is returned, the size of
-    % an image is the number of 1's in the mask
-%     allWarped = zeros(endIndex-startIndex+1,sum(mask(:)));
-    downsize_mask = imresize(mask, 0.5);
-    allWarped = zeros(endIndex-startIndex+1, size(downsize_mask, 1)*size(downsize_mask,2));
-
-    images = mpie.allExamplesColour(startIndex:endIndex,:,:);
     
-    allWarped(1:endIndex-startIndex+1,:) = WarpImages(images, landmarkLocations(startIndex:endIndex,:,:), T, triX, mask, alphas, betas, npix, xmin, ymin, 0.5);
+    textures = WarpImages(images, landmarkLocations, T, triX, mask, alphas, betas, npix, xmin, ymin, 1)/255;
 
 end
--- Matlab code/WarpImages.m
+++ Matlab code/WarpImages.m
@@ -1,18 +1,15 @@
-function [warpeds] = WarpImages( images, landmarkLocations,  T, triX, mask, alphas, betas,npix,xmin, ymin, downsize_factor)
-    % Only store the pixels that are in the mask, so the size is the
-    % number of 1's in the mask
-%     warpeds = zeros(size(images,1), sum(mask(:)));
-    downsize_mask = imresize(mask, downsize_factor);
-    warpeds = zeros(size(images,1), size(downsize_mask,1)*size(downsize_mask,2));
+function [textures] = WarpImages( images, landmarkLocations,  T, triX, mask, alphas, betas,npix,xmin, ymin)
+
+    textures = zeros(size(images,1), sum(mask(:)));
     numImages = size(images,1);
     
     for i=1:numImages
         image = images(i,:,:);
         imageLandmarks = squeeze(landmarkLocations(i,:,:));
-        warpIm = Crop(squeeze(image), imageLandmarks, T, triX, mask, alphas, betas,npix,xmin, ymin);
-        warpIm = imresize(warpIm, downsize_factor);
+        texture = Crop(squeeze(image), imageLandmarks, T, triX, mask, alphas, betas,npix,xmin, ymin);
         % Vectorise the image and store it in the list of warped images and
         % remove the zero pixels that don't need to be stored
-        warpeds(i,:) = warpIm(:);
+        texture_vec = texture(:);
+        textures(i,:) = texture_vec(logical(mask(:)));
     end
 end
